9ce6fbcd7c8ed5cff253e1bfa934d0cc2555da7c
Weroance <danielrjansen@protonmail.com>
2024-04-18 15:30:46 -0400

    Add: everything. via if else if.

--- rpc.js
+++ rpc.js
@@ -3,35 +3,39 @@
 // paper > rock
 
 //  get user choice
-function playerChoice(a) {
-    return a.toUpperCase();
-}
 
 let computerChoice = 0
 
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
-    switch (choice){
+    switch (Math.floor(Math.random() * 3))  {
         case 0:
-            computerChoice = "SCISSORS"
-        
+            computerChoice = "SCISSORS" ;
+            break;
         case 1:
-            computerChoice = "PAPER" 
-        
+            computerChoice = "PAPER" ;
+            break;
         case 2:
-            computerChoice = "ROCK"
-
-    }
+            computerChoice = "ROCK" ;
+            break;
 }
-
+}
 // get random computer choice 
 // convert it to rock paper or scissors
-
-function Play(userChoice, computerChoice)   {
-// compare choices declare winner or stalemate
-}
-
-//let getComputerChoice = () => Math.random();
-//
-//getComputerChoice
-//console.log(getComputerChoice())
+function Play(player, computer){
+    let playerChoice = player.toUpperCase()
+      if (playerChoice == computer) {
+        return 'STALEMATE';
+      }
+      else if ((playerChoice === 'ROCK' && computer === 'PAPER') 
+      || (playerChoice === 'PAPER' && computer === 'SCISSORS') 
+      || (playerChoice === 'SCISSORS' && computer === 'ROCK'))    {
+        return 'you Lose!';
+      }
+        else  {
+        return 'You Win!';
+      }
+      
+    }
+getComputerChoice() 
+console.log(computerChoice)
+console.log(Play('paper', computerChoice))
